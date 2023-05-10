10698cfdd48ffb44397444136c8fd004283d8439
stackk1 <astackk@gmail.com>
2023-05-09 17:15:28 -0700

    add GameTabView as start point tag tabs

--- Casino/Views/GameTabView.swift
+++ Casino/Views/GameTabView.swift
@@ -8,28 +8,29 @@
 import SwiftUI
 
 struct GameTabView: View {
+    @State var tabIndex = 3
     var body: some View {
-        TabView{
+        TabView(selection: $tabIndex){
             WarView()
                 .tabItem{
                     Image(systemName: "cross.fill")
                     Text("War")
-                }
+                }.tag(0)
             SlotView()
                 .tabItem{
                     Image(systemName: "bitcoinsign.circle.fill")
                     Text("Slots")
-                }
+                }.tag(1)
             DiceView()
                 .tabItem{
                     Image(systemName: "die.face.6")
                     Text("Dice")
-                }
+                }.tag(2)
             CasinoView()
                 .tabItem{
                     Image(systemName: "list.bullet")
                     Text("All Games")
-                }
+                }.tag(3)
         }
     }
 }
